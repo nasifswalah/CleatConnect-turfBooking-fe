12fc4026dc3c62899e1bfb833232ae495b166a75
nasifswalah <nasifswalah@gmail.com>
2024-06-19 13:43:22 +0530

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
   server:{
     proxy:{
       '/api' : {
-        target: 'http://localhost:5000',
+        target: 'https://cleat-connect-turf-booking-backend.vercel.app/',
         secure: false
       }
     }
   },
   plugins: [react()],
 })
+
