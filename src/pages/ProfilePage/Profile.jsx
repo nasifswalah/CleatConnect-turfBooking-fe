5ad64a8db71fcee125d3ab65479245e7dfd94b18
nasifswalah <nasifswalah@gmail.com>
2024-06-26 16:32:57 +0530

    Completed the profile card

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -1,40 +1,50 @@
-import React from 'react'
-import './Profile.css'
-import { useSelector } from 'react-redux';
-import Navbar from '../../components/Navbar/Navbar';
+import React from "react";
+import "./Profile.css";
+import cameraIcon from "../../assets/cameraIcon.svg";
+import addUserIcon from "../../assets/addUserIcon.svg"
+import showCourtsIcon from "../../assets/showCourts.svg"
+import addNewCourtIcon from "../../assets/addNewCourt.svg"
+import showMyCourtIcon from "../../assets/showMyCourtIcon.svg"
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Profile = () => {
-
-    const { currentUser } = useSelector((state) => state.user);
-
   return (
     <>
     <Navbar/>
-    <div className="profile-container">
-      <h1 className="profile-text">Profile</h1>
-      <form  className="profile-data">
-        <input
-          type="text"
-          placeholder="username"
-          defaultValue={currentUser.data.name}
-          id="username"
-        />
-        <input
-          type="email"
-          placeholder="email"
-          defaultValue={currentUser.data.email}
-          id="email"
-        />
-        <input
-          type="text"
-          placeholder="role"
-          defaultValue={currentUser.data.role}
-          id="role"
-        />
-      </form>
+    <div className="card-container">
+      <div className="card">
+        <div className="card-header">
+          <div className="card-main">
+            <div className="profile-image">
+              <div className="upload-image">
+                <img src={cameraIcon} alt="camera icon" />
+              </div>
+            </div>
+            <h3 className="username">Nasif</h3>
+            <p className="user-subname">User</p>
+          </div>
+        </div>
+        <div className="card-content">
+          <div className="left-content">
+            <div className="profile-container">
+              <h3 className="detail-title">Email</h3>
+              <p className="detail-data">Nasif</p>
+              <h3 className="detail-title">Mobile Number</h3>
+              <p className="detail-data">1234567890</p>
+            </div>
+            <div className="buttons-container">
+              <Link to='' className="icon"><img src={addUserIcon} alt="" /></Link>
+              <Link to='' className="icon"><img src={addNewCourtIcon} alt="" /></Link>
+              <Link to='' className="icon"><img src={showCourtsIcon} alt="" /></Link>
+              <Link to='' className="icon"><img src={showMyCourtIcon} alt="" /></Link>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   );
-}
+};
 
-export default Profile
+export default Profile;
